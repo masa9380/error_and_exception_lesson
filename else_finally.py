7985247408e198545eede02bd96bf2b127441ad5
masa9380 <def.aa33@gmail.com>
2025-09-13 19:53:55 +0900

    try_except, else_finally, raise, myerror.pyを追加

--- else_finally.py
+++ else_finally.py
@@ -2,14 +2,17 @@ def split_bill(price):
     num = input('何人で割り勘しますか？')
     try:
         each = price / int(num)
+        return 'try'
     except ZeroDivisionError as e:
         print('0で割ることはできません。正しい値を入力してください。')
-        each = price
         print(e)
     except ValueError:
         print('数字を入力してください')
-        each = price
-    print(f'一人{each}円です')
+    else:
+        print(f'一人{each}円です')
+    finally:
+        print('ご利用ありがとうございます。')
+        return 'finally'
 
 if __name__ == "__main__":
     split_bill(10000)
